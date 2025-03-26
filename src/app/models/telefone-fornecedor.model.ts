e330f4b6e5cc1652d656952eb38a974a521f112b
Hallan Christian <hallanchristiancruz@gmail.com>
2025-03-26 11:06:01 -0300

    Crud Fornecedor Atualizado

---
+++ src/app/models/telefone-fornecedor.model.ts
@@ -0,0 +1,8 @@
+import { Fornecedor } from "./fornecedor.model";
+
+export class TelefoneFornecedor {
+    id!: number;
+    codigoArea!: string;
+    numero!: string;
+    fornecedor!: Fornecedor;
+}
